00e014b7e2019a63488ad69e873c17a1de89cbd5
Tim Dohmen <dohmentim@yahoo.com>
2020-02-03 13:13:49 -0700

    fixed hiscore

--- server-assets/routes/runelite.js
+++ server-assets/routes/runelite.js
@@ -9,10 +9,7 @@ var itemExt = axios.create({
   baseURL: 'https://raw.githubusercontent.com/tanlines/powerbot-scripts/master/items_useful_data.json',
   timeout: 3000
 })
-var newExt = axios.create({
-  baseURL: 'https://secure.runescape.com',
-  timeout: 6000
-})
+
 router.get('/api/runelite/:id', async (req, res, next) => {
   let response = await ext.get("m=itemdb_oldschool/api/catalogue/detail.json?item=" + req.params.id)
   res.send(response.data)
@@ -22,7 +19,7 @@ router.get('/api/runelite', async (req, res, next) => {
   res.send(response.data)
 })
 router.put('/api/runelite/hiscore', async (req, res, next) => {
-  let response = await ext.get("m=hiscore_oldschoolindex_lite.ws?player=" + req.body.name)
+  let response = await ext.get("m=hiscore_oldschool/index_lite.ws?player=" + req.body.name)
   res.send(response.data)
 })
 
