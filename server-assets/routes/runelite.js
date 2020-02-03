9ad6542119ca421a6899baf40f87c9e6292632e5
Tim Dohmen <dohmentim@yahoo.com>
2020-02-03 13:41:39 -0700

    changed to secure

--- server-assets/routes/runelite.js
+++ server-assets/routes/runelite.js
@@ -9,7 +9,10 @@ var itemExt = axios.create({
   baseURL: 'https://raw.githubusercontent.com/tanlines/powerbot-scripts/master/items_useful_data.json',
   timeout: 3000
 })
-
+var hiScoreExt = axios.create({
+  baseURL: 'https://secure.runescape.com/m=hiscore_oldschool',
+  timeout: 8000
+})
 router.get('/api/runelite/:id', async (req, res, next) => {
   let response = await ext.get("m=itemdb_oldschool/api/catalogue/detail.json?item=" + req.params.id)
   res.send(response.data)
@@ -19,8 +22,8 @@ router.get('/api/runelite', async (req, res, next) => {
   res.send(response.data)
 })
 router.put('/api/runelite/hiscore', async (req, res, next) => {
-  let response = await ext.get("m=hiscore_oldschool/index_lite.ws?player=" + req.body.name)
-  res.send(response.data)
+  let response = await hiScoreExt.get("index_lite.ws?player=" + req.body.name)
+  res.send(response.data || response.error)
 })
 
 
